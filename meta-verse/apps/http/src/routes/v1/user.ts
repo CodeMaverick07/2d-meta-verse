373a807bd6ee0c16ef0fc032c995dc8f9608e669
HemantJatal <hemantjatal59@gmail.com>
2025-03-30 17:00:34 +0530

    add initial project setup

---
+++ meta-verse/apps/http/src/routes/v1/user.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+
+
+export const userRouter = Router();
+
+userRouter.post("/metadata", async (req, res) => {})
+
+userRouter.get("/metadata/bluk", async (req, res) => {})
