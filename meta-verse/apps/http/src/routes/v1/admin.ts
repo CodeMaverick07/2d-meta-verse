f51aa10401faf26265268c90e147c870567d6edd
Turbobot <turbobot@vercel.com>
2025-03-30 18:17:53 +0530

    feat(create-turbo): apply official-starter transform

--- meta-verse/apps/http/src/routes/v1/admin.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-
-export const adminRouter = Router();
-// "/api/v1/admin"
-adminRouter.post("/element", (req, res) => {});
-adminRouter.put("/element/:elementId", (req, res) => {});
-adminRouter.get("/avatar", (req, res) => {});
-adminRouter.get("/map", (req, res) => {});
--- meta-verse/apps/http/src/routes/v1/user.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-
-
-export const userRouter = Router();
-
-userRouter.post("/metadata", async (req, res) => {})
-
-userRouter.get("/metadata/bluk", async (req, res) => {})
