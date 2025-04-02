55f7b60799fff84990f2a88c320395f816f35b65
HemantJatal <hemantjatal59@gmail.com>
2025-04-03 02:53:20 +0530

    feat(ws): implement RoomManager and User classes for WebSocket handling and user management

---
+++ meta-verse/apps/ws/src/config.ts
@@ -0,0 +1,11 @@
+export const getRandomString = (length: number) => {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+};
+
+export const JWT_PASSWORD = "verysecret";
--- meta-verse/apps/ws/src/index.ts
+++ meta-verse/apps/ws/src/index.ts
@@ -1,11 +1,14 @@
 import { WebSocketServer } from "ws";
+import { User } from "./User";
 
 const wss = new WebSocketServer({ port: 3001 });
-wss.on("connection", (ws) => {
-  console.log("New client connected");
+
+wss.on("connection", function connection(ws) {
+  console.log("yser connected");
+  let user = new User(ws);
   ws.on("error", console.error);
 
   ws.on("close", () => {
-    console.log("Client disconnected");
+    user?.destroy();
   });
 });
